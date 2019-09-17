3279beac7f6ae37d404c44d84eb8b5ec18108d68
hngfu <djsl1018@naver.com>
2019-09-17 16:34:52 +0900

    가위바위보 결과창 추가

--- JS game/RSP game/RSP.js
+++ JS game/RSP game/RSP.js
@@ -5,6 +5,7 @@ var rsp = [rock, scissors, paper];
 var index = 0;
 
 var enemy = document.querySelector('#enemy');
+var resultView = document.querySelector('#result');
 
 function makeInterval() {
     return setInterval(() => {
@@ -19,9 +20,9 @@ var buttons = document.querySelectorAll('.rsp');
 buttons.forEach(btn => {
     btn.addEventListener('click', e => {
         var clickedIndex = Array.from(e.target.parentNode.children).indexOf(e.target);
-        if (clickedIndex === index) console.log('비겼습니다--');
-        else if ([-1, 2].includes(clickedIndex - index)) console.log('이겼습니다~!@#');
-        else console.log('졌.잘.싸ㅠ');
+        if (clickedIndex === index) resultView.textContent = '비겼습니다--';
+        else if ([-1, 2].includes(clickedIndex - index)) resultView.textContent = '이겼습니다~!@#';
+        else resultView.textContent = '졌.잘.싸ㅠ';
         clearInterval(interval);
         setTimeout(() => {
             interval = makeInterval();
