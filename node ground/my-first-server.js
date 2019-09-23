d2b2b08539bac44947a90f36b59b21974103ede3
hngfu <djsl1018@naver.com>
2019-09-23 17:45:17 +0900

    쿠키 추가

--- node ground/my-first-server.js
+++ node ground/my-first-server.js
@@ -1,13 +1,23 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     fs.readFile('./index.html', (err, data) => {
+        console.log('cookie: ', req.headers.cookie, req.url);
         if (err) {
             throw err;
         }
         res.end(data);
+        res.writeHead(200, {
+            'Set-Cookie': 'hngfu cookie = delicious'
+        });
     })
-}).listen(8080, () => {
-    console.log('서버가 실행되었습니다.');
-})
+}).listen(8080);
+
+server.on('listening', () => {
+    console.log('블리츠크랭크 왈: 인간 시대의 끝이 도래했다.');
+});
+
+server.on('error', (error) => {
+    console.error(error);
+});
