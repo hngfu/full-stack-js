75f97182e66a0c28cfae669f5990d133395ca996
hngfu <djsl1018@naver.com>
2019-09-24 15:01:02 +0900

    302 응답, 쿠키 사용

--- node ground/my-first-server.js
+++ node ground/my-first-server.js
@@ -1,17 +1,28 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
 
 const server = http.createServer((req, res) => {
-    fs.readFile('./index.html', (err, data) => {
-        console.log('cookie: ', req.headers.cookie, req.url);
-        if (err) {
-            throw err;
-        }
-        res.end(data);
-        res.writeHead(200, {
-            'Set-Cookie': 'hngfu cookie = delicious'
-        });
-    })
+    if (req.url.startsWith('/login')) {
+        fs.readFile('./server1.html', (err, data) => {
+            const { query } = url.parse(req.url);
+            const { name } = qs.parse(query);
+            const expires = new Date();
+            expires.setMinutes(expires.getMinutes() + 5);
+            res.writeHead(302, {
+                Location: '/',
+                'Set-Cookie': `name = ${encodeURIComponent(name)}; Expires=${expires.toUTCString()}; HttpOnly; Path = /`
+            })
+            res.end(data);
+        })
+    } else if (req.headers.cookie) {
+        res.end(`Cookie info: ${req.headers.cookie}`);
+    } else {
+        fs.readFile('./server2.html', (err, data) => {
+            res.end(data);
+        })
+    }
 }).listen(8080);
 
 server.on('listening', () => {
